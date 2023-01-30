cdf5d972313d2b032d85b78a679a663a8c5ba784
Hannah Wolfe <github.erisds@gmail.com>
2023-01-30 17:01:59 +0000

    Get capacitor working with yarn dev

--- packages/koenig-lexical/capacitor.config.ts
+++ packages/koenig-lexical/capacitor.config.ts
@@ -4,7 +4,10 @@ const config: CapacitorConfig = {
     appId: 'org.ghost.koenig.demo',
     appName: 'Koenig Demo',
     webDir: 'dist',
-    bundledWebRuntime: false
+    bundledWebRuntime: false,
+    server: {
+        url: 'http://127.0.0.1:5173/'
+    }
 };
 
 export default config;
